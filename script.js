3f74a100a554eae64d4b3f2223eec0b1bf6c6839
MiroslavKop <kopytovmiroslav@gmail.com>
2022-08-07 14:44:49 +0300

    задача №1

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strick";
+
+// Задача №1---------------------------------------/
+
+let result;
+function sumTwoSmallestNumbers(array) {
+  let copyArray = [...array];
+  let copyArraySort = copyArray.sort((a, b) => a - b);
+  result =
+    copyArray.length < 2
+      ? console.error("...")
+      : copyArraySort[0] + copyArraySort[1];
+  return result;
+}
+console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77]));
+console.log(sumTwoSmallestNumbers([10, 343, 345, 3, 13, 22]));
+console.log(sumTwoSmallestNumbers([1]));
