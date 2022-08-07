982a38c246ce61283443d99e1a2c6188fa597b51
MiroslavKop <kopytovmiroslav@gmail.com>
2022-08-07 16:39:28 +0300

    задача №2

--- script.js
+++ script.js
@@ -2,16 +2,42 @@
 
 // Задача №1---------------------------------------/
 
-let result;
-function sumTwoSmallestNumbers(array) {
-  let copyArray = [...array];
-  let copyArraySort = copyArray.sort((a, b) => a - b);
-  result =
-    copyArray.length < 2
-      ? console.error("...")
-      : copyArraySort[0] + copyArraySort[1];
-  return result;
-}
-console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77]));
-console.log(sumTwoSmallestNumbers([10, 343, 345, 3, 13, 22]));
-console.log(sumTwoSmallestNumbers([1]));
+// let result;
+// function sumTwoSmallestNumbers(array) {
+//   let copyArray = [...array];
+//   let copyArraySort = copyArray.sort((a, b) => a - b);
+//   result =
+//     copyArray.length < 2
+//       ? console.error("...")
+//       : copyArraySort[0] + copyArraySort[1];
+//   return result;
+// }
+// console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77]));
+// console.log(sumTwoSmallestNumbers([10, 343, 345, 3, 13, 22]));
+// console.log(sumTwoSmallestNumbers([1]));
+
+// Задача №2---------------------------------------/
+
+// function createCalculator(x) {
+//   let count = x;
+//   return {
+//     sum: function (y) {
+//       return (count = count + y);
+//     },
+//     mult: function (y) {
+//       return (count = count * y);
+//     },
+//     min: function (y) {
+//       return (count = count - y);
+//     },
+//     div: function (y) {
+//       return (count = count / y);
+//     },
+//   };
+// }
+
+// const calc = createCalculator(10);
+// console.log(calc.sum(5));
+// console.log(calc.mult(10));
+// console.log(calc.min(40));
+// console.log(calc.div(10));
